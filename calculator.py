a6b9321802d8b77a93117aa98d182366d61305d2
Miki <62766191+Milenko-crypto@users.noreply.github.com>
2021-11-19 20:55:36 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+class Calculator:
+    def SumfromNumbers(self,first_number, second_number): # zbog klase ova f postaje metoda
+        result = first_number+second_number
+        return result
+
+
+
